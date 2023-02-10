91ff5eb00a07e2f5559eb4dd3cb3f965644e3d8d
philosoph10 <77683432+philosoph10@users.noreply.github.com>
2023-02-10 16:31:09 +0200

    voice recognition
    recognizes voice and if the confidence is above the threshold (0.7) then the output is fed to toxicity recognizer

--- bot.py
+++ bot.py
@@ -3,12 +3,15 @@ import telebot
 from dotenv import load_dotenv
 from utils.ai import predict
 from googletrans import Translator
+import speech_recognition as sr
+from pydub import AudioSegment
 
 load_dotenv()
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
 translator = Translator()
+speech_recognizer = sr.Recognizer()
 
 
 @bot.message_handler(commands=['start', 'hello'])
@@ -24,6 +27,43 @@ def echo_all(message):
         bot.reply_to(message, 'toxicity detected: ' + str(prob))
     else:
         bot.reply_to(message, prob)
+
+
+@bot.message_handler(content_types=['voice'])
+def handle_voice_message(message):
+    raw = message.voice.file_id
+    src_path = "utils/downloaded_files/" + raw + ".ogg"
+    dest_path = "utils/downloaded_files/" + raw + ".wav"
+    file_info = bot.get_file(raw)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(src_path, 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    AudioSegment.from_file(src_path).export(dest_path, format='wav')
+
+    with sr.AudioFile(dest_path) as source:
+        # listen for the data (load audio to memory)
+        audio_data = speech_recognizer.record(source)
+        # recognize (convert from speech to text)
+        try:
+            text, conf = speech_recognizer.recognize_google(audio_data, with_confidence=True)
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+            return
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+            return
+        print("Confidence = {}".format(conf))
+        # text = translator.translate(text).text
+        if conf <= 0.7:
+            return
+        prob = predict(text)[0][1]
+        if prob > 0.7:
+            bot.reply_to(message, 'toxicity detected: ' + str(prob))
+        else:
+            bot.reply_to(message, prob)
+        os.remove(src_path)
+    os.remove(dest_path)
 
 
 bot.infinity_polling()
